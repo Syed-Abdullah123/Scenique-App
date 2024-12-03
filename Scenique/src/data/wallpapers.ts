3bd10067f6ade41dbea4c3aea418df6e1af03d6a
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-03 12:47:03 +0500

    Added dummy wallpapers data

---
+++ Scenique/src/data/wallpapers.ts
@@ -0,0 +1,34 @@
+export const wallpapers = [
+  {
+    id: 1,
+    title: "Wallpaper 1",
+    image: require("../../assets/images/art.jpg"),
+    authorName: "User 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 2,
+    title: "Wallpaper 2",
+    image: require("../../assets/images/art2.jpg"),
+    authorName: "User 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 3,
+    title: "Wallpaper 3",
+    image: require("../../assets/images/art3.jpg"),
+    authorName: "User 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 4,
+    title: "Wallpaper 4",
+    image: require("../../assets/images/art4.jpg"),
+    authorName: "User 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+];
