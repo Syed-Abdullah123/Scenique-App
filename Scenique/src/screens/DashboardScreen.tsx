cabd21dd582a4d775e52cc39088f08d1aca84bc5
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-03 12:43:43 +0500

    Added dummy wallpapers data

--- Scenique/src/screens/DashboardScreen.tsx
+++ Scenique/src/screens/DashboardScreen.tsx
@@ -1,10 +1,43 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  TouchableHighlight,
+} from "react-native";
+import { wallpapers } from "../data/wallpapers";
+import LoaderAnimation from "../components/CustomLoader";
 
-const DashboardScreen = () => {
+const DashboardScreen = ({ navigation }) => {
+  const renderItem = ({ item }: any) => {
+    return (
+      <TouchableHighlight
+        style={styles.flatlistContainer}
+        onPress={() => navigation.navigate("Details", { item })}
+      >
+        <View>
+          <Image source={item.image} style={styles.image}></Image>
+          <View style={styles.overlay}>
+            <Text style={styles.title}>{item.title}</Text>
+            {/* <Text style={styles.author}>{item.authorName}</Text> */}
+          </View>
+        </View>
+        {/* <Text style={styles.desc}>{item.description}</Text> */}
+      </TouchableHighlight>
+    );
+  };
+
+  if (!wallpapers) return <LoaderAnimation />;
+
   return (
     <View style={styles.container}>
-      <Text>DashboardScreen</Text>
+      <FlatList
+        data={wallpapers}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 };
@@ -14,7 +47,41 @@ export default DashboardScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 10,
+    backgroundColor: "#363B40",
+  },
+  flatlistContainer: {
+    marginVertical: 10,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  title: {
+    fontFamily: "CG_Bold",
+    fontSize: 24,
+    color: "#fff",
+    textAlign: "center",
+  },
+  image: {
+    width: "100%",
+    height: 120,
+  },
+  author: {
+    fontFamily: "Lexend_Bold",
+    fontSize: 16,
+    color: "#fff",
+  },
+  desc: {
+    fontFamily: "Lexend_Regular",
+    fontSize: 12,
+    color: "#e0e0e0",
   },
 });
