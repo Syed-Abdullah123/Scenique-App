68c5ac996212cec70bcaad43bf4c707b8a2d5498
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-03 19:48:22 +0500

    Added navigation for liked images

--- Scenique/src/screens/LikesScreen.tsx
+++ Scenique/src/screens/LikesScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import LoaderAnimation from "../components/CustomLoader";
+import LikeScreenComponent from "../components/LikeScreenComponent";
 
-const LikesScreen = () => {
+const LikesScreen = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
-      <LoaderAnimation />
+      <LikeScreenComponent navigation={navigation} />
     </View>
   );
 };
@@ -15,8 +15,6 @@ export default LikesScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#363B40",
   },
   text: {
