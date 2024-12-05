a99b6b197cfcc030b676d0dfd2ecdecec1468126
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-05 11:34:38 +0500

    Added more screens to the navigator

--- Scenique/src/navigation/RootNavigator.tsx
+++ Scenique/src/navigation/RootNavigator.tsx
@@ -1,7 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import TabNavigator from "./TabNavigator";
 import DetailsScreen from "../screens/DetailsScreen";
+import CategoryDetails from "../screens/CategoryDetailsScreen";
+import AllCategories from "../screens/AllCategoriesScreen";
 
 const RootNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -10,6 +13,8 @@ const RootNavigator = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={TabNavigator} />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Cat_Details" component={CategoryDetails} />
+        <Stack.Screen name="AllCategories" component={AllCategories} />
       </Stack.Navigator>
     </NavigationContainer>
   );
