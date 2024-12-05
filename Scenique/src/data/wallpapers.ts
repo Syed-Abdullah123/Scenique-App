751e404440aa557fac4bce84cbad0886ce890039
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-05 11:34:05 +0500

    Added more wallpapers

--- Scenique/src/data/wallpapers.ts
+++ Scenique/src/data/wallpapers.ts
@@ -1,7 +1,7 @@
 export const wallpapers = [
   {
     id: 1,
-    title: "Wallpaper 1",
+    title: "Abstract",
     image: require("../../assets/images/art.jpg"),
     authorName: "User 1",
     description:
@@ -9,7 +9,7 @@ export const wallpapers = [
   },
   {
     id: 2,
-    title: "Wallpaper 2",
+    title: "Nature",
     image: require("../../assets/images/art2.jpg"),
     authorName: "User 2",
     description:
@@ -17,7 +17,7 @@ export const wallpapers = [
   },
   {
     id: 3,
-    title: "Wallpaper 3",
+    title: "Historic",
     image: require("../../assets/images/art3.jpg"),
     authorName: "User 3",
     description:
@@ -25,8 +25,40 @@ export const wallpapers = [
   },
   {
     id: 4,
-    title: "Wallpaper 4",
+    title: "Space",
     image: require("../../assets/images/art4.jpg"),
+    authorName: "User 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 5,
+    title: "Fantasy",
+    image: require("../../assets/images/art2.jpg"),
+    authorName: "User 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 6,
+    title: "Patterns",
+    image: require("../../assets/images/art3.jpg"),
+    authorName: "User 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 7,
+    title: "Sports",
+    image: require("../../assets/images/art4.jpg"),
+    authorName: "User 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
+  },
+  {
+    id: 8,
+    title: "Vehicles",
+    image: require("../../assets/images/art.jpg"),
     authorName: "User 4",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
