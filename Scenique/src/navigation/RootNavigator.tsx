b31c100f35bf34fe6e843aaecb6a846c4e306f08
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-07 12:26:02 +0500

    Removed AllCategoriesScreen from Stack

--- Scenique/src/navigation/RootNavigator.tsx
+++ Scenique/src/navigation/RootNavigator.tsx
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "./TabNavigator";
 import DetailsScreen from "../screens/DetailsScreen";
 import CategoryDetails from "../screens/CategoryDetailsScreen";
-import AllCategories from "../screens/AllCategoriesScreen";
 
 const RootNavigator = () => {
   const Stack = createNativeStackNavigator();
@@ -14,7 +13,6 @@ const RootNavigator = () => {
         <Stack.Screen name="Home" component={TabNavigator} />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Cat_Details" component={CategoryDetails} />
-        <Stack.Screen name="AllCategories" component={AllCategories} />
       </Stack.Navigator>
     </NavigationContainer>
   );
