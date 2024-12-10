3b5febe3e7e5231d5cadce7bfc94f8da8819b2bb
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-10 10:51:17 +0500

    Added correct paths for wallpapers

--- Scenique/src/data/wallpapers.ts
+++ Scenique/src/data/wallpapers.ts
@@ -2,7 +2,7 @@ export const wallpapers = [
   {
     id: 1,
     title: "Abstract",
-    image: require("../../assets/images/art.jpg"),
+    image: require("../../assets/images/abstract.jpg"),
     authorName: "User 1",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
@@ -26,7 +26,7 @@ export const wallpapers = [
   {
     id: 4,
     title: "Space",
-    image: require("../../assets/images/art4.jpg"),
+    image: require("../../assets/images/space.jpg"),
     authorName: "User 4",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
@@ -34,7 +34,7 @@ export const wallpapers = [
   {
     id: 5,
     title: "Fantasy",
-    image: require("../../assets/images/art2.jpg"),
+    image: require("../../assets/images/fantasy.jpg"),
     authorName: "User 1",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
@@ -42,7 +42,7 @@ export const wallpapers = [
   {
     id: 6,
     title: "Patterns",
-    image: require("../../assets/images/art3.jpg"),
+    image: require("../../assets/images/patterns.jpg"),
     authorName: "User 2",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
@@ -50,7 +50,7 @@ export const wallpapers = [
   {
     id: 7,
     title: "Sports",
-    image: require("../../assets/images/art4.jpg"),
+    image: require("../../assets/images/sports.jpg"),
     authorName: "User 3",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
@@ -58,7 +58,7 @@ export const wallpapers = [
   {
     id: 8,
     title: "Vehicles",
-    image: require("../../assets/images/art.jpg"),
+    image: require("../../assets/images/vehicles.jpg"),
     authorName: "User 4",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, dolores soluta, quibusdam rerum velit. Doloribus, dolores soluta, quibusdam rerum velit.",
