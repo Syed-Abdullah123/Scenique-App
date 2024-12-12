9e1d71afc2172c164c5fd5d41f65c11c1cb30760
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-12 11:27:13 +0500

    Adjusted Button stylings

--- Scenique/src/components/ButtonComponent.tsx
+++ Scenique/src/components/ButtonComponent.tsx
@@ -12,7 +12,7 @@ const ButtonComponent: React.FC<ButtonProps> = ({ title, onPress, icon }) => {
   return (
     <Pressable style={styles.button} onPress={onPress}>
       <Text style={styles.buttonText}>{title}</Text>
-      <Ionicons name={icon} size={24} color="#32BAE8" />
+      <Ionicons name={icon} size={24} color="#363B40" style={{ top: 1 }} />
     </Pressable>
   );
 };
@@ -21,21 +21,17 @@ export default ButtonComponent;
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "#e0e0e0",
+    backgroundColor: "#32BAE8",
     padding: 12,
     borderRadius: 5,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     gap: 5,
-    position: "absolute",
-    bottom: "10%",
-    left: 20,
-    right: 20,
   },
   buttonText: {
-    color: "#000",
+    color: "#363B40",
     fontSize: 16,
-    fontFamily: "CG_Regular",
+    fontFamily: "CG_Medium",
   },
 });
