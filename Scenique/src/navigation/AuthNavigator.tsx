365095736add3ee07ab407efc8bd23de5ee6a495
Syed-Abdullah123 <sp21-bcs-037@cuiatk.edu.pk>
2024-12-12 11:29:30 +0500

    Created auth navigator with screens

---
+++ Scenique/src/navigation/AuthNavigator.tsx
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Welcome from "../screens/WelcomeScreen";
+import Signup from "../screens/SignupScreen";
+import Signin from "../screens/SigninScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{ animation: "slide_from_right" }}
+      />
+      <Stack.Screen
+        name="Signin"
+        component={Signin}
+        options={{ animation: "slide_from_right" }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthNavigator;
